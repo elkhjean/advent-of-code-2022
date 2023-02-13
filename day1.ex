dbd367ad65291e5511b653f754864f3a36df3333
Jean El Khoury <sean.elkhoury@gmail.com>
2023-02-13 11:09:02 +0100

    removed unused/old code

--- day1.ex
+++ day1.ex
@@ -8,16 +8,8 @@ defmodule Day1 do
     collective_inv = String.split(File.read!("resources/calinv.txt"), "\r\n\r\n")
     collective_inv = Enum.map(collective_inv, fn part -> String.split(part) end)
     collective_inv = Enum.map(collective_inv, fn part -> Enum.map(part, fn n -> {the_int, _} = Integer.parse(n); the_int end) end)
-    collective_inv = sum_indiv(collective_inv)
+    collective_inv = Enum.map(collective_inv, fn indiv_inv -> Enum.sum(indiv_inv) end)
     Enum.sort(collective_inv, :desc)
-  end
-
-  def sum_indiv(inv) do
-    Enum.map(inv, fn indiv_inv -> Enum.sum(indiv_inv) end)
-  end
-
-  def max_inv() do
-    Enum.max(read_inventory())
   end
 
   def sum_top_n(n) do
